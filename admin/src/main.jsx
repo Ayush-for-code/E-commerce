037a39523c6797d6124eb5ee48297ede2c221102
Ayush-for-code <ayushbhardwajgdr@gmail.com>
2026-08-30 10:43:23 +0530

    make admin uri work

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./state/store.js";
 import { ClerkProvider } from "@clerk/react";
-const clerkPubKey = import.meta.env.CLERK_PUBLISHABLE_KEY;
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 console.log(
   "CLERK KEY:",
   clerkPubKey ? "LOADED" : "MISSING"
